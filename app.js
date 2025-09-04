8d41cadb0841df465bc3836c9c34bcc850b8a636
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-09-04 22:34:37 +0600

    create new tour

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// Middleware to parse JSON bodies
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello Server is Running....' })
 })
@@ -20,6 +23,20 @@ app.get('/api/v1/tours', (req, res) => {
     data: {
       tours,
     },
+  })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1]._id + 1
+  const newTour = Object.assign({ id: newId }, req.body)
+  tours.push(newTour)
+  fs.writeFile(`${__dirname}/data/tours.json`, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    })
   })
 })
 
