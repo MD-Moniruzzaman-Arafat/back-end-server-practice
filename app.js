643d1664c89b4ea54b7aea9d9cdfc9a1f4e02026
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-09-05 16:39:42 +0600

    create users crud operation

--- app.js
+++ app.js
@@ -80,6 +80,41 @@ const deleteTour = (req, res) => {
   })
 }
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  })
+}
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  })
+}
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  })
+}
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  })
+}
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  })
+}
+
 // app.get('/api/v1/tours', getAllTours)
 // app.get('/api/v1/tours/:id', getTour)
 // app.post('/api/v1/tours', createTour)
@@ -88,8 +123,10 @@ const deleteTour = (req, res) => {
 
 // 3) Routes
 app.route('/api/v1/tours').get(getAllTours).post(createTour)
-
 app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour)
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser)
+app.route('/api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser)
 
 // 4) Start Server
 app.listen(PORT, () => {
