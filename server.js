0db8381396818ffa6be42dc4cdcf8dc44bdf8a94
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-09-06 11:04:35 +0600

    now we create a schema and model

--- server.js
+++ server.js
@@ -14,6 +14,27 @@ mongoose
   .then(() => console.log('DB connection successful!'))
   .catch((err) => console.log('DB connection error:', err))
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+    trim: true,
+    maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+    minlength: [10, 'A tour name must have more or equal then 10 characters'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+})
+
+const Tour = mongoose.model('Tour', tourSchema)
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
